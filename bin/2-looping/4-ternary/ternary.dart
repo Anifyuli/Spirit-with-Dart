5145545044b895be297458b4183a2c792bfffb28
Anifyuli <anifyuliansyah@gmail.com>
2023-04-21 09:17:11 +0700

    Making exercise & commit List on Dart

--- bin/2-looping/4-ternary/ternary.dart
+++ bin/2-looping/4-ternary/ternary.dart
@@ -1,5 +1,3 @@
-import 'package:test/test.dart';
-
 void main() {
   var myAge = 20;
   myAge >= 20 ? print("You're adult") : print('Sorry, you not allowed to whatch this');
--- bin/4-collection/1-list/list.dart
+++ bin/4-collection/1-list/list.dart
@@ -30,5 +30,66 @@ void main() {
   // Mengecek apakah list kosong atau tidak
   print('Is no animal here? It is ${animal.isEmpty.toString()}');
 
-  
+  // Reversed List
+  print("Reversed Animal List : ${animal.reversed}");
+
+  // Menambahkan item ke list
+  myList.add(13);
+  print("MyList after add() : $myList");
+
+  // Menambahkan banyak item ke list dengan addAll
+  myList.addAll([3, 7, 22, 34]);
+  print("MyList after addAll() : $myList");
+
+  // Metode lain menambahkan item ke list dengan insert()
+  myList.insert(0, 90);
+  print("MyList after insert() : $myList");
+
+  // Metode lain menambahkan banyak item ke list dengan insertAll()
+  myList.insertAll(1, [33, 44, 55]);
+  print("MyList after insertAll() : $myList");
+
+  // Mengganti item dengan jangkauan yang ditetapkan dengan replaceRange()
+  myList.replaceRange(2, 5, [9, 16, 72, 88]);
+  print("MyList after replaceRange() : $myList");
+
+  // Menghapus sebuah item pada list dengan remove()
+  myList.remove(3);
+  print("MyList after remove a 3 item : $myList");
+
+  // Menghapus item pada list berdasarkan indeks dengan removeAt()
+  myList.removeAt(3);
+  print("MyList after removeAt(3rd index) : $myList");
+
+  // Menghapus item terakhir list dengan removeLast()
+  myList.removeLast();
+  print("MyList after removeLast() : $myList");
+
+  // Menghapus item dengan jangkauan yang ditetapkan pada list menggunakan removeRange()
+  myList.removeRange(6, 10);
+  print("MyList after removeRange() : $myList");
+
+  // Membuat perulangan pada list dengan for()
+  print("Looping list with for() : ");
+  for (int i = 0; i < myList.length; i++) {
+    print("$i index is : ${myList[i]}");
+  }
+
+  // Mengalikan semua item pada list dengan list.map()
+  var multipleTwoMyList = myList.map((n) => n * 2);
+  print("MyList multiple by 2 : $multipleTwoMyList");
+
+  // Menggabungkan dua list
+  List hundred = [100, 200, 300, 400];
+  List allNum = [...myList, ...hundred];
+  print("AllNum : $allNum");
+
+  // Pengkondisian list
+  bool bitter = true;
+  var drink = ['milk', 'tea', if (bitter) 'herb'];
+  print("Drink product list : $drink");
+
+  // Melakukan sortir pada List dengan where()
+  List evenNum = myList.where((myNum) => myNum.isEven).toList();
+  print("MyList filtering result with where() : $evenNum");
 }
